baab9e58e4fd6be3e75be83f05af1b86e056d061
Mathew Blair <mblair@student.unimelb.edu.au>
2013-05-10 14:06:46 +1000

    making sure

---
+++ src/gameworlds/GLaDOS.java
@@ -0,0 +1,16 @@
+package gameworlds;
+// Genetic Lifeform and Disk Operating System
+// i.e. stats watcher
+
+public class GLaDOS {
+	
+	private float leveltime;
+	private int levelid;
+	
+	public GLaDOS(){
+		
+	}
+	
+	
+
+}
