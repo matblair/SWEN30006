84fa0635a839aa7cd4ea21ebc7d88b3b2855041d
jrfitzsimmons <jrfitzsimmons@gmail.com>
2013-05-26 17:54:01 +1000

    More work on switches

--- src/gameobjects/BigSwitch.java
+++ src/gameobjects/BigSwitch.java
@@ -1,21 +1,14 @@
 package gameobjects;
 
-import java.util.ArrayList;
-
 import gameengine.PhysUtils;
 import gamestates.GameState;
 
-import org.jbox2d.collision.shapes.PolygonShape;
 import org.jbox2d.common.Vec2;
-import org.jbox2d.dynamics.Body;
-import org.jbox2d.dynamics.BodyDef;
-import org.jbox2d.dynamics.BodyType;
 import org.jbox2d.dynamics.FixtureDef;
 import org.jbox2d.dynamics.World;
 import org.jbox2d.dynamics.contacts.ContactEdge;
 import org.jbox2d.dynamics.joints.*;
 
-import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
 
 public class BigSwitch {
@@ -23,10 +16,10 @@ public class BigSwitch {
 	private static final String SENSORIMAGE = "BIGSWITCHSENSOR";
 	private static final String BASESHAPE = "BIGSWITCHBASE";
 	private static final String SENSORSHAPE = "BIGSWITCHSENSOR";
-	private static final int BUTTONOFFSET = 11;
+	private static final int BUTTONOFFSET = 9;
+	private static final int BUTTONTRANSLATION = 4;
 
 	private GameObject base, button, sensor;
-	private ArrayList<GameObject> renderableGO;
 	private String doorId;
 	private Door doorlink=null;
 
@@ -39,9 +32,6 @@ public class BigSwitch {
 		base = new GameObject(BASEIMAGE);
 		button = new GameObject(SENSORIMAGE);
 		sensor = new GameObject();
-		renderableGO = new ArrayList<GameObject>();
-		renderableGO.add(base);
-		renderableGO.add(button);
 
 		// Create the sensors
 		FixtureDef basefd = base.createFixture(BASESHAPE);
@@ -63,17 +53,21 @@ public class BigSwitch {
 		PrismaticJointDef jd = new PrismaticJointDef();
 		jd.initialize(button.getBody(), base.getBody(), button.getLocation(), axis);
 		jd.enableLimit = true;
-		jd.lowerTranslation = -3f;
-		jd.upperTranslation = 4f;
-		jd.motorSpeed = 10000;
-		jd.maxMotorForce = 0.1f;
+		jd.lowerTranslation = 0f;
+		jd.upperTranslation = PhysUtils.pixelsToMetres(BUTTONTRANSLATION);
+		jd.motorSpeed = -10;
+		jd.maxMotorForce = 10;
 		jd.enableMotor = true;
 		jd.collideConnected=false;
 		world.createJoint(jd);
 	}
 	
-	public ArrayList<GameObject> getRenderableComponents() {
-		return renderableGO;
+	public GameObject getBase() {
+		return base;
+	}
+	
+	public GameObject getButton() {
+		return button;
 	}
 	
 	public String getBodyID() {
